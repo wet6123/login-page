be34db109bf93c70186887a5393f01043c25084e
JunSeong Bae <gentlmanjun@naver.com>
2021-10-01 17:32:03 +0900

    로그인 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,13 +2,22 @@
 
 const id = document.querySelector("#idInput");
 const pw = document.querySelector("#pwInput");
-const loginButton = document.querySelector("button");
+const loginButton = document.querySelector("#button");
 
 loginButton.addEventListener("click", login);
 
-function login() {
+function login(event) {
+  event.preventDefault();
   const req = {
     id: id.value,
     pw: pw.value,
   };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "appication/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
