eb9020881f5aad8b0ec81e0e2de1a5bf4374d08f
JunSeong Bae <gentlmanjun@naver.com>
2021-10-01 13:54:34 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.use(express.static(`${__dirname}/src/public`));
+app.use(express.static(`${__dirname}/src/public`)); //public의 js파일과 views의 ejs파일을 연결시켜주기 위한 미들웨어
 app.use("/", home); //use는 미들 웨어를 등록해주는 매서드
 
 module.exports = app;
