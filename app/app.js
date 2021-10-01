8ccfbd63358ce035cbf07cfe8f8a68c132da6d27
JunSeong Bae <gentlmanjun@naver.com>
2021-10-01 13:24:57 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); //use는 미들 웨어를 등록해주는 매서드
+
+module.exports = app;
