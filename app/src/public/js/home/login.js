b0b7236b97531234375bd0632b751bb6a1c40dc1
JunSeong Bae <gentlmanjun@naver.com>
2021-10-05 21:24:36 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -21,5 +21,14 @@ function login(event) {
     body: JSON.stringify(req),
   })
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 에러 발생"));
+    });
 }
