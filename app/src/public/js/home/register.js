97d3f6abe5dd81bf7e6cfcda693e3fe09a4a32b7
JunSeong Bae <gentlmanjun@naver.com>
2021-11-04 00:56:01 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -10,11 +10,16 @@ registerButton.addEventListener("click", register);
 
 function register(event) {
   event.preventDefault();
+
+  if (!id.value) return alert("아이디를 입력해주십시오.");
+
+  if (pw.value !== confirmPw.value)
+    return alert("비밀번호가 일치하지 않습니다.");
+
   const req = {
     id: id.value,
     name: name.value,
     pw: pw.value,
-    confirmPw: confirmPw.value,
   };
 
   fetch("/register", {
